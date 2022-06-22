f83eb90fdab4e89a22846f59f6fb2941cf879549
Oleksii Datsiuk <Olmine2013@gmail.com>
2022-06-22 12:30:03 +0300

    #2: Added starting code for the login and register pages

---
+++ src/components/register_page/register_page.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+
+export class RegisterPage extends React.Component{
+    render() {
+        return (
+            <div>
+                <h1>Register Page</h1>
+            </div>
+        )
+    }
+}
