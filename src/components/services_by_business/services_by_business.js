56cdc0a855854be9764f23b0c8db1f2eb935ab71
YaroslavBorysko <yaroslavborysko@gmail.com>
2022-07-11 13:05:14 +0300

    #319: added js file

---
+++ src/components/services_by_business/services_by_business.js
@@ -0,0 +1,49 @@
+import React from "react";
+import axios from 'axios';
+import Table from 'react-bootstrap/Table';
+import {useEffect, useState} from "react";
+import {useParams} from "react-router-dom";
+
+import './services_by_business.css';
+
+const ServiceByBusiness = () => {
+
+    const params = useParams()
+
+    let [data, setDate] = useState([])
+
+    useEffect(() => {
+
+        axios.get(`https://g6bcybbjx1.execute-api.eu-central-1.amazonaws.com/api/v1/business/${params.id}/services`)
+            .then(res => {
+                setDate(res.data.results)
+            }).catch(err => console.log(err))
+    }, [params.id])
+
+    const arr = data.map((elem, index) => {
+        return (
+            <tr key={index}>
+                <td>{elem.name}</td>
+                <td>{elem.price}</td>
+                <td>{elem.description}</td>
+                <td>{elem.duration}</td>
+            </tr>
+        )
+    })
+    return (
+        <Table striped bordered hover>
+            <thead>
+            <tr>
+                <th>Name</th>
+                <th>Price</th>
+                <th>Description</th>
+                <th>Duration</th>
+            </tr>
+            </thead>
+            <tbody>
+            {arr}
+            </tbody>
+        </Table>
+    )
+}
+export default ServiceByBusiness
