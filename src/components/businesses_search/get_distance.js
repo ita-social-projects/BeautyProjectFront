5a7e81e0f55dc580affd36b8369ecebb17957736
therobotisnotatoy <pythonshumor@gmail.com>
2022-07-14 16:21:01 +0300

    #310: Improved distance calculation

--- src/components/businesses_search/get_distance.js
+++ src/components/businesses_search/get_distance.js
@@ -1,14 +1,19 @@
 
 export function getDistance(lat_t, lon_t, lat_b, lon_b) {
-    const p = 0.017453292519943295;    // Math.PI / 180
 
-    let degree = 0.5 - Math.cos((lat_b - lat_t) * p) / 2 + Math.cos(lat_t * p) * Math.cos(lat_b * p) *
-        (1 - Math.cos((lon_b - lon_t) * p)) / 2;
-    let result = 12742 * Math.asin(Math.sqrt(degree))
+    let R = 6378.137; // Radius of earth in KM
+    let dLat = lat_b * Math.PI / 180 - lat_t * Math.PI / 180;
+    let dLon = lon_b * Math.PI / 180 - lon_t * Math.PI / 180;
+    let a = Math.sin(dLat/2) * Math.sin(dLat/2) +
+        Math.cos(lat_t * Math.PI / 180) * Math.cos(lat_b * Math.PI / 180) *
+        Math.sin(dLon/2) * Math.sin(dLon/2);
+    let c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
+    let d = R * c;
+    let result = d * 1000; // meters
 
     let precision = 100
-    if (result < 200) {
-        precision = 10
+    if (result < 300) {
+        precision = 20
     }
 
     return Math.round(result / precision) * precision
